5d3b2947ed38dc7c2ed405635ea2c13698fb1d11
Stenbyte <sten.scheifel@gmail.com>
2021-08-05 23:17:29 +0300

    header, menuOpen

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Portfolio() {
-  return <div className="portfolio"></div>;
+  return <div className="portfolio" id="portfolio"></div>;
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,32 @@
 import React from "react";
 import "./topbar.scss";
-export default function Topbar() {
-  return <div className="topbar"></div>;
+import { Person, Mail } from "@material-ui/icons";
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Developer
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+358406687250</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>sten.scheifel@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
