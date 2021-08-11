47e60185ea8ccf540344660b53452229378d9918
Stenbyte <sten.scheifel@gmail.com>
2021-08-11 08:31:09 +0300

    text type animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import "./intro.scss";
+import { init } from "ityped";
 
 export default function Intro() {
-  return <div className="intro" id="intro"></div>;
+  const text = useRef();
+  useEffect(() => {
+    init(text.current, {
+      showCursor: true,
+      startDelay: 1000,
+      backDelay: 1500,
+      backSpeed: 100,
+      typeSpeed: 100,
+      strings: ["Developer"],
+    });
+  }, []);
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/me.jpg" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Sten Scheifel</h1>
+          <h3>
+            Frontend <span ref={text}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
