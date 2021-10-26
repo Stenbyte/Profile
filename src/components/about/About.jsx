94115edefa31b4399cffff5c8c8b1b26deb3d5a4
Stenbyte <sten.scheifel@gmail.com>
2021-10-26 13:38:22 +0300

    grammar fixes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,23 +14,22 @@ export default function About({ num }) {
       <div className="parallax" style={style}></div>
       <div className="container" style={style1}>
         <p>
-          • Since 2018, I knew my calling was in Software Developmnet
+          • Since 2018, I knew my calling was in Software Development
           <br />
           • I have read many topics about computers and everything that is
           related to them. Tried many boot camps, free courses, tutorials. Wrote
           some dummy pages, apps.
           <br />
-          • January 2020, I applied for Full Stack Web Development
-          Programm(React and Nodejs majors) at Business College Helsinki and was
-          accepted
+          • January 2020, I applied for Full Stack Web Development Program(React
+          and Nodejs majors) at Business College Helsinki and was accepted
           <br />
           • It is a full-time 17-months intensive program, which covers both
-          front and backend
+          Front and Backend
           <br />• A lot of projects, teamwork, coding, and learning 24/7
-          <br /> • I chose to stick with Frontend because it's always evolving
-          and constantly changing.
+          <br /> • I chose to stick with Frontend because of big interest
+          towards it.
           <br />
-          • Since June 2021, I'm a Frontend developer intern at Siciliamia
+          • Since June 2021, I'm a Frontend Developer intern at Siciliamia
           startup.
           <br />
           • These studies and internship has boosted my development skills
