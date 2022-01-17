8debf2e2acd56c760bcf97f5fe78ec01b62769ab
Stenbyte <sten.scheifel@gmail.com>
2022-01-17 15:45:23 +0200

    intro changed

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,23 +1,44 @@
 import React from "react";
 import "./intro.scss";
+import cloud from "../../img/cloud.svg";
+import plus from "../../img/plus.svg";
+import dots from "../../img/dots.svg";
+import star from "../../img/star.svg";
+import st from "../../img/st.svg";
 
 export default function Intro() {
   return (
     <div className="intro" id="intro">
-      <div className="top">
-        <div className="top1">
-          <h3>
-            Hi, I'm <span>Sten Scheifel.</span> Frontend Developer
-          </h3>
+      <div className="left">
+        <img src={cloud} alt="" className="cloud" />
+        <img src={plus} alt="" className="plus" />
+        <img src={dots} alt="" className="dots" />
+        <div className="leftTop">
+          <h1>
+            I'm <span>Sten Scheifel</span>.
+            <br />
+            I'm Frontend Developer.
+          </h1>
+          <p>
+            I like to create, animate frontend products with great user
+            experiences.
+          </p>
         </div>
-        <p>
-          “Success is not final; failure is not fatal: It is the courage to
-          continue that counts.”
-        </p>
-        <span className="span">-- Winston S. Churchill</span>
+        <div className="leftBottom">
+          <p>I specialise in React, Javascript and Typescript</p>
+          <p>
+            Highly motivated to become <br />
+            Full-Stack Developer
+          </p>
+        </div>
       </div>
-      <div className="bottom">
-        <img src="assets/st.png" alt="" />
+      <div className="center">
+        <img src={st} alt="" className="me" />
+      </div>
+      <div className="right">
+        <img src={plus} alt="" className="plus1" />
+        <img src={dots} alt="" className="dots1" />
+        <img src={star} alt="" className="star" />
       </div>
     </div>
   );
